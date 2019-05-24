69effc502cdc21103896762c26d4eece2cffc2a6
gagahwahwa <wjdrkghk123@naver.com>
2019-05-24 20:10:04 +0900

    db수정

--- db_config.js
+++ db_config.js
@@ -1,6 +1,7 @@
 module.exports = {
     host: '127.0.0.1',
     user: 'testuser',
+    port:'3306',
     password: '1234',
     database: 'twice'
 }
--- index.js
+++ index.js
@@ -14,30 +14,32 @@ app.listen(app.get('port'), () => console.log('Twice server listening on port ' 
 
 var con = mysql.createConnection(db_config);
 
+con.query('SET GLOBAL connect_timeout=28800');
+con.query('SET GLOBAL wait_timeout=28800');
+con.query('SET GLOBAL interactive_timeout=28800');
+
 //글 저장
 app.post('/booking', (req, res) => {
-    console.log(req.originalUrl);
+    //console.log(req.originalUrl);
 
     var id = req.body.user_id;
     var token = req.body.token;
     var message = req.body.message;
     var bookingTime = req.body.bookingTime;
     // ... --> video
-
-    var query = `INSERT INTO booking (uid, token, bookingTime, message)
-            VALUES (${id}, ${token}, ${bookingTime}, ${message})`;
+    var query = `INSERT INTO booking (uid, token, bookingTime, message) VALUES (${id}, ${token}, ${bookingTime}, ${message})`;
     console.log(query);
-    
     con.query(query, (err) => {
+        console.log('inserting query');
         if(err) {
-            res.send({
+            res.json({
                 status: 'fail',
                 result: err,
             });
         } else {
-            res.json('success');
+            res.send('success');
         }
-    });
+    })    
 });
 
 

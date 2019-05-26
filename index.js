9bf1672c6a3bbef318a3dfb1600f1d8940d428f3
gagahwahwa <wjdrkghk123@naver.com>
2019-05-26 17:42:34 +0900

    i don't know
    qwe

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mysql = require('mysql');
 const express = require('express');
 const db_config = require('./db_config');
 const bodyParser = require('body-parser');
-const schedule = require('node-schedule');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,7 +25,8 @@ app.post('/booking', (req, res) => {
     var token = req.body.token;
     var message = req.body.message;
     var bookingTime = req.body.bookingTime;
-    // ... --> video
+
+    // photo 있을 때 없을 때 나누기
     var query = `INSERT INTO booking (uid, token, bookingTime, message) VALUES ('${id}', '${token}', '${bookingTime}', '${message}')`;
     console.log(query);
     con.query(query, (err) => {
@@ -41,59 +41,3 @@ app.post('/booking', (req, res) => {
         }
     })    
 });
-
-
-
-
-// 예약 전송
-// var j = schedule.scheduleJob('* 30 * * * *', function() {
-//     console.log(moment().format('YYYY-MM-DD-HH:mm'));
-//     var query = `SELECT token, message, image
-//                 FROM booking 
-//                 WHERE bookingTime = ${mysql.escape(moment().format('YYYY-MM-DD-HH:mm'))}`;
-
-//     conn.query(query).then(result => {
-//         for (let i = 0; i < result.length; i++) {
-//             // 데이터 분할
-//             let token = result[i].token;
-//             let message = result[i].message;
-//             if(result[i].image) {
-//                 let image = result[i].image;
-//             }
-
-//             // 전송
-//             postingFacebook(token, message, image);
-//         }
-//     }).catch(error => {
-//         console.log('Post Upload fail' + moment().format('YYYY-MM-DD-HH:mm'));
-//     });
-// });
-
-
-function postingFacebook(token, message, image = null) {
-    var url = `/feed/?message=${message}&access_token=${token}`;
-    var resurl = encodeURI(url);
-    var options = {
-        host: 'graph.facebook.com',
-        port: 443,
-        path: resurl,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-    };
-
-    var httpreq = https.request(options, function (httpres) {
-        var data = '';
-        httpres.setEncoding('utf8');
-
-        httpres.on('data', function (chunk) {
-            console.log(chunk);
-            data += chunk;
-        })
-        httpres.on('end', function () {
-            res.send(data);
-        })
-    });
-    httpreq.end()
-}
